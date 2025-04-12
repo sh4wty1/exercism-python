1117ba141ce6de96f78970237359f55598861b7f
sh4wty1 <notlucasfa@gmail.com>
2025-04-12 15:43:04 -0300

    Add Exercism Python exercises

---
+++ pangram/app.py
@@ -0,0 +1,9 @@
+import string
+
+
+def is_pangram(sentence):
+    letters = set()
+    for i in sentence.lower():
+        if i in string.ascii_lowercase:
+            letters.add(i)
+    return len(letters) == 26
